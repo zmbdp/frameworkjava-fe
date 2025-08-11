75cc9975dce915ba92f70dff226bf42a22e91108
稚名不带撇 <3566983494@qq.com>
2025-08-11 21:16:35 +0800

    Java脚手架前端

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    //通过配置 alias 来定义路径的别名
+    alias: {
+      // __dirname: 当前文件所在目录的绝对路径
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+});
