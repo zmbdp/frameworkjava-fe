38f143b1862eff6000b1f2b691e7e396db85ddec
稚名不带撇 <3566983494@qq.com>
2025-08-12 23:16:16 +0800

    Java脚手架前端

---
+++ src/views/Login/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>登录页面</div>
+</template>
+
+<script setup></script>
+
+<script>
+  export default {
+    name: 'LoginPage'
+  };
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/System/Account/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>平台账号管理</div>
+</template>
+
+<script setup></script>
+
+<script>
+  export default {
+    name: 'AccountPage'
+  };
+</script>
+
+<style lang="scss" scoped></style>
