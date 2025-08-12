6cef40299987665c4e868dc7e340b95c09297a6e
稚名不带撇 <3566983494@qq.com>
2025-08-12 23:18:42 +0800

    Java脚手架前端

---
+++ src/views/System/Dictionary/Detail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>字典详情</div>
+</template>
+
+<script setup></script>
+
+<script>
+  export default {
+    name: 'DictionaryDetail'
+  };
+</script>
+
+<style lang="scss" scoped></style>
