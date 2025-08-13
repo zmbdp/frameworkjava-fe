6b27ba39596d7a9b076f8fe7db2ffd29094d37ae
稚名不带撇 <3566983494@qq.com>
2025-08-13 21:50:21 +0800

    Java脚手架前端

---
+++ src/api/login.js
@@ -0,0 +1,26 @@
+// 导入公共请求函数
+import request from '@/utils/request'
+
+/**
+ * 登录
+ */
+export const loginApi = ({ phone, password }) => {
+  return request({
+    method: 'post',
+    url: '/admin/sys_user/login/password',
+    data: {
+      phone,
+      password
+    }
+  })
+}
+
+/**
+ * 获取用户信息
+ */
+export const getUserInfoApi = () => {
+  return request({
+    method: 'get',
+    url: '/admin/sys_user/login_info/get'
+  })
+}
